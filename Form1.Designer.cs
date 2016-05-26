fa52ccca3550d5b8e486b4dfbe8bd384552d0495
TongYao Si <846510629@qq.com>
2016-05-26 15:19:45 +0800

    first_version

---
+++ Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace osproject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.memory_panel = new System.Windows.Forms.Panel();
+            this.algorithm_choose_box = new System.Windows.Forms.ComboBox();
+            this.next_step_button = new System.Windows.Forms.Button();
+            this.input_size_label = new System.Windows.Forms.Label();
+            this.memory_text = new System.Windows.Forms.TextBox();
+            this.submit_button = new System.Windows.Forms.Button();
+            this.algorithm_choose_label = new System.Windows.Forms.Label();
+            this.instruction_displayer = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // memory_panel
+            // 
+            this.memory_panel.Location = new System.Drawing.Point(12, 12);
+            this.memory_panel.Margin = new System.Windows.Forms.Padding(0);
+            this.memory_panel.Name = "memory_panel";
+            this.memory_panel.Size = new System.Drawing.Size(399, 443);
+            this.memory_panel.TabIndex = 0;
+            this.memory_panel.Paint += new System.Windows.Forms.PaintEventHandler(this.memory_panel_Paint);
+            // 
+            // algorithm_choose_box
+            // 
+            this.algorithm_choose_box.FormattingEnabled = true;
+            this.algorithm_choose_box.Items.AddRange(new object[] {
+            "首次适应算法",
+            "最佳适应算法"});
+            this.algorithm_choose_box.Location = new System.Drawing.Point(478, 130);
+            this.algorithm_choose_box.Name = "algorithm_choose_box";
+            this.algorithm_choose_box.Size = new System.Drawing.Size(121, 20);
+            this.algorithm_choose_box.TabIndex = 1;
+            this.algorithm_choose_box.Text = "最佳适应算法";
+            this.algorithm_choose_box.SelectedIndexChanged += new System.EventHandler(this.algorithm_choose_box_SelectedIndexChanged);
+            // 
+            // next_step_button
+            // 
+            this.next_step_button.Location = new System.Drawing.Point(498, 156);
+            this.next_step_button.Name = "next_step_button";
+            this.next_step_button.Size = new System.Drawing.Size(75, 23);
+            this.next_step_button.TabIndex = 2;
+            this.next_step_button.Text = "next step";
+            this.next_step_button.UseVisualStyleBackColor = true;
+            this.next_step_button.Click += new System.EventHandler(this.start_button_Click);
+            // 
+            // input_size_label
+            // 
+            this.input_size_label.AutoSize = true;
+            this.input_size_label.Location = new System.Drawing.Point(445, 47);
+            this.input_size_label.Name = "input_size_label";
+            this.input_size_label.Size = new System.Drawing.Size(191, 12);
+            this.input_size_label.TabIndex = 3;
+            this.input_size_label.Text = "Please input total memory size:";
+            // 
+            // memory_text
+            // 
+            this.memory_text.Location = new System.Drawing.Point(488, 62);
+            this.memory_text.Name = "memory_text";
+            this.memory_text.Size = new System.Drawing.Size(100, 21);
+            this.memory_text.TabIndex = 4;
+            this.memory_text.Text = "640";
+            // 
+            // submit_button
+            // 
+            this.submit_button.Location = new System.Drawing.Point(498, 89);
+            this.submit_button.Name = "submit_button";
+            this.submit_button.Size = new System.Drawing.Size(75, 23);
+            this.submit_button.TabIndex = 5;
+            this.submit_button.Text = "submit";
+            this.submit_button.UseVisualStyleBackColor = true;
+            this.submit_button.Click += new System.EventHandler(this.submit_button_Click);
+            // 
+            // algorithm_choose_label
+            // 
+            this.algorithm_choose_label.AutoSize = true;
+            this.algorithm_choose_label.Location = new System.Drawing.Point(462, 115);
+            this.algorithm_choose_label.Name = "algorithm_choose_label";
+            this.algorithm_choose_label.Size = new System.Drawing.Size(161, 12);
+            this.algorithm_choose_label.TabIndex = 6;
+            this.algorithm_choose_label.Text = "Please choose an algorithm";
+            // 
+            // instruction_displayer
+            // 
+            this.instruction_displayer.Location = new System.Drawing.Point(436, 196);
+            this.instruction_displayer.Name = "instruction_displayer";
+            this.instruction_displayer.Size = new System.Drawing.Size(200, 243);
+            this.instruction_displayer.TabIndex = 7;
+            this.instruction_displayer.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 467);
+            this.Controls.Add(this.instruction_displayer);
+            this.Controls.Add(this.algorithm_choose_label);
+            this.Controls.Add(this.submit_button);
+            this.Controls.Add(this.memory_text);
+            this.Controls.Add(this.input_size_label);
+            this.Controls.Add(this.next_step_button);
+            this.Controls.Add(this.algorithm_choose_box);
+            this.Controls.Add(this.memory_panel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel memory_panel;
+        private System.Windows.Forms.ComboBox algorithm_choose_box;
+        private System.Windows.Forms.Button next_step_button;
+        private System.Windows.Forms.Label input_size_label;
+        private System.Windows.Forms.TextBox memory_text;
+        private System.Windows.Forms.Button submit_button;
+        private System.Windows.Forms.Label algorithm_choose_label;
+        private System.Windows.Forms.RichTextBox instruction_displayer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace osproject
+{
+    public partial class Form1 : Form
+    {
+        private int memory_size = 640;
+        private System.Collections.ArrayList freespace = new System.Collections.ArrayList();
+        private System.Collections.ArrayList usedspace = new System.Collections.ArrayList();
+        //private FileStream instrction = new FileStream("instrction.txt", FileMode.OpenOrCreate, FileAccess.Read);
+        private StreamReader instruction = new StreamReader("instruction.txt");
+        //private Algorithm method = Algorithm.BESTFIT;
+        bool started = false;
+
+        private void repaintPanel()
+        {
+            Graphics g = memory_panel.CreateGraphics();
+            memory_panel.Show();
+            for(int freeindex = 0;freeindex < freespace.Count;freeindex++)
+            {
+                SpaceNode nowNode = (SpaceNode)freespace[freeindex];
+
+                g.FillRectangle((Brush)Brushes.Green,
+                0, (int)((double)(nowNode.begin - 1) / memory_size * memory_panel.Height),
+                memory_panel.Width, (int)((double)nowNode.capacity / memory_size * memory_panel.Height));
+
+                g.DrawLine(new Pen(Color.Black, 2),
+                    new Point(0, (int)((double)nowNode.end / memory_size * memory_panel.Height)),
+                    new Point(memory_panel.Width, (int)((double)nowNode.end / memory_size * memory_panel.Height)));
+
+                g.DrawString(String.Format("FREE MEMORY:{0} BYTE", nowNode.capacity),
+                    Font, (Brush)Brushes.Black,
+                    memory_panel.Width / 2 - 50,
+                    (int)((double)(nowNode.begin - 1) / memory_size * memory_panel.Height) + (int)((double)nowNode.capacity / memory_size * memory_panel.Height) / 2 - 4);
+            }
+
+            for(int usedindex = 0;usedindex < usedspace.Count;usedindex++)
+            {
+                SpaceNode nowNode = (SpaceNode)usedspace[usedindex];
+
+                g.FillRectangle((Brush)Brushes.Red,
+                0, (int)((double)(nowNode.begin - 1) / memory_size * memory_panel.Height),
+                memory_panel.Width, (int)((double)nowNode.capacity / memory_size * memory_panel.Height));
+
+                g.DrawLine(new Pen(Color.Black, 2),
+                    new Point(0, (int)((double)nowNode.end / memory_size * memory_panel.Height)),
+                    new Point(memory_panel.Width, (int)((double)nowNode.end / memory_size * memory_panel.Height)));
+
+                g.DrawString(String.Format("{0} USE MEMORY:{1} BYTE", nowNode.jobname, nowNode.capacity),
+                    Font, (Brush)Brushes.Black,
+                    memory_panel.Width / 2 - 60,
+                    (int)((double)(nowNode.begin - 1) / memory_size * memory_panel.Height) + (int)((double)nowNode.capacity / memory_size * memory_panel.Height) / 2 - 4);
+            }
+                            
+        }
+        public Form1()
+        {
+            freespace.Add(new SpaceNode(1, memory_size, memory_size));
+            InitializeComponent();
+        }
+
+        private void memory_panel_Paint(object sender, PaintEventArgs e)
+        {
+            repaintPanel();
+        }
+
+        private void algorithm_choose_box_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void start_button_Click(object sender, EventArgs e)
+        {
+            string algorithm_string = algorithm_choose_box.Text;
+            if (!algorithm_string.Equals("首次适应算法") && !algorithm_string.Equals("最佳适应算法"))
+            {
+                MessageBox.Show("请选择首次适应算法或最佳适应算法！");
+                return;
+            }
+            string s = instruction.ReadLine();
+            if(s == null)
+            {
+                MessageBox.Show("文件已读取完毕！");
+                return;
+            }
+            Regex pattern = new Regex(@"^(?<jobname>\w+) (?<operation>alloc|free) (?<size>\d+)$");
+            if(!pattern.IsMatch(s))
+            {
+                MessageBox.Show("无效指令，请检查文件！");
+                return;
+            }
+            if (!started)
+            {
+                started = true;
+            }
+
+            Match match = pattern.Match(s);
+            string jobname = match.Groups["jobname"].Value;
+            string operation = match.Groups["operation"].Value;
+            int size = Convert.ToInt32(match.Groups["size"].Value);
+            
+
+            if(operation.Equals("alloc"))
+            {
+                for(int i = 0;i < usedspace.Count;i++)
+                {
+                    if(((SpaceNode)usedspace[i]).jobname.Equals(jobname))
+                    {
+                        MessageBox.Show(String.Format("指令{0}非法！已有同名任务存在于内存中，将执行下一条指令",s));
+                        instruction_displayer.Text += s + " [failed]" + '\n';
+                        return;
+                    }
+                }
+                Algorithm method = algorithm_string.Equals("首次适应算法") ? Algorithm.FIRSTFIT : Algorithm.BESTFIT;
+                int index = -1;
+                if(method == Algorithm.FIRSTFIT)
+                {
+                    int begin = memory_size + 10;
+                    for(int i = 0;i < freespace.Count;i++)
+                    {
+                        if(((SpaceNode)freespace[i]).capacity >= size && ((SpaceNode)freespace[i]).begin < begin)
+                        {
+                            begin = ((SpaceNode)freespace[i]).begin;
+                            index = i;
+                        }
+                    }
+                }
+                if(method == Algorithm.BESTFIT)
+                {
+                    int min_capacity = memory_size + 10;
+                    for(int i = 0;i < freespace.Count;i++)
+                    {
+                        if(((SpaceNode)freespace[i]).capacity >= size && ((SpaceNode)freespace[i]).capacity < min_capacity)
+                        {
+                            min_capacity = ((SpaceNode)freespace[i]).capacity;
+                            index = i;
+                        }
+                    }
+                }
+                if (index == -1)
+                {
+                    MessageBox.Show(String.Format("内存不足，指令{0}执行失败，将执行下一条指令",s));
+                    instruction_displayer.Text += s + " [failed]" + '\n';
+                    return;
+                }
+
+                usedspace.Add(new SpaceNode(((SpaceNode)freespace[index]).begin, ((SpaceNode)freespace[index]).begin + size - 1, size, jobname));
+                ((SpaceNode)freespace[index]).capacity -= size;
+                ((SpaceNode)freespace[index]).begin += size;
+                if(((SpaceNode)freespace[index]).begin == ((SpaceNode)freespace[index]).end + 1)
+                    freespace.RemoveAt(index);
+                
+                
+            }
+
+            if(operation.Equals("free"))
+            {
+                int usedindex = -1;
+                for(int i = 0;i < usedspace.Count;i++)
+                {
+                    if(((SpaceNode)usedspace[i]).jobname.Equals(jobname))
+                    {
+                        usedindex = i;
+                        break;
+                    }
+                }
+                if(usedindex == -1)
+                {
+                    MessageBox.Show(String.Format("在内存中寻找不到{0}，指令{1}执行失败，将执行下一条指令", jobname,s));
+                    instruction_displayer.Text += s + " [failed]" + '\n';
+                    return;
+                }
+
+                int freeindex = -1;
+                for(int i = 0;i < freespace.Count;i++)
+                {
+                    if(((SpaceNode)usedspace[usedindex]).begin == ((SpaceNode)freespace[i]).end + 1 ||
+                        ((SpaceNode)usedspace[usedindex]).end + 1 == ((SpaceNode)freespace[i]).begin)
+                    {
+                        freeindex = i;
+                        break;
+                    }
+                }
+
+                if(freeindex == -1)
+                {
+                    freespace.Add(new SpaceNode(((SpaceNode)usedspace[usedindex]).begin,
+                        ((SpaceNode)usedspace[usedindex]).end, ((SpaceNode)usedspace[usedindex]).capacity));
+                }
+                else
+                {
+                    if(((SpaceNode)usedspace[usedindex]).begin == ((SpaceNode)freespace[freeindex]).end + 1)
+                    {
+                        ((SpaceNode)freespace[freeindex]).end = ((SpaceNode)usedspace[usedindex]).end;
+                        ((SpaceNode)freespace[freeindex]).capacity += ((SpaceNode)usedspace[usedindex]).capacity;
+                    }
+                    else if(((SpaceNode)usedspace[usedindex]).end + 1 == ((SpaceNode)freespace[freeindex]).begin)
+                    {
+                        ((SpaceNode)freespace[freeindex]).begin = ((SpaceNode)usedspace[usedindex]).begin;
+                        ((SpaceNode)freespace[freeindex]).capacity += ((SpaceNode)usedspace[usedindex]).capacity;
+                    }
+                }
+                usedspace.RemoveAt(usedindex);
+                freespace.Sort(new SortWithBeginTime());
+                for(int i = 1;i < freespace.Count;i++)
+                {
+                    SpaceNode preNode = (SpaceNode)freespace[i - 1];
+                    SpaceNode nowNode = (SpaceNode)freespace[i];
+                    if(preNode.end + 1 == nowNode.begin)
+                    {
+                        preNode.end = nowNode.end;
+                        preNode.capacity += nowNode.capacity;
+                        freespace.RemoveAt(i);
+                        i--;
+                    }
+                }
+            }
+            memory_panel.CreateGraphics().Clear(Color.FromArgb(255, 255, 255, 255));
+            repaintPanel();
+            instruction_displayer.Text += s + '\n';
+        }
+
+        private void submit_button_Click(object sender, EventArgs e)
+        {
+            if (started)
+            {
+                MessageBox.Show("程序已开始，无法重新指定内存大小！");
+                return;
+            }
+            int memory = 0;
+            try
+            {
+                memory = Convert.ToInt32(memory_text.Text);
+            }
+             catch(Exception exce)
+            {
+                MessageBox.Show("Invaild Input!");
+                return;
+            }
+            
+            memory_size = memory;
+            freespace.Clear();
+            usedspace.Clear();
+            freespace.Add(new SpaceNode(1, memory_size, memory_size));
+            memory_panel.CreateGraphics().Clear(Color.FromArgb(255,255,255,255));
+            repaintPanel();
+        }
+    }
+    public class SpaceNode
+    {
+        public int begin, end;
+        public int capacity;
+        public string jobname;
+        public SpaceNode(int _begin,int _end,int _capacity)
+        {
+            begin = _begin;
+            end = _end;
+            capacity = _capacity;
+        }
+        public SpaceNode(int _begin, int _end, int _capacity, string _jobname)
+        {
+            begin = _begin;
+            end = _end;
+            capacity = _capacity;
+            jobname = _jobname;
+        }
+    }
+    public enum Algorithm
+    {
+        FIRSTFIT,
+        BESTFIT
+    }
+    public class SortWithBeginTime : System.Collections.IComparer
+    {
+        public int Compare(object a,object b)
+        {
+            return ((SpaceNode)a).begin - ((SpaceNode)b).begin;
+        }
+    }
+}
